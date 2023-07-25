232857bbf5b82b36a2b831b1fb7ecbf965092177
Serhii Maksymchuk <java.lead@prof-it.bz>
2023-07-25 21:56:57 +0300

    Implemented cancellation of asset deletion

--- src/core/components/assets/AssetDeleteWindow/index.js
+++ src/core/components/assets/AssetDeleteWindow/index.js
@@ -7,20 +7,29 @@ export default function AssetDeleteWindow(assetToDelete, setAssetToDelete,) {
             ? `${assetToDelete.currency} ` : "";
     }
 
+    const cancel = () => {
+        setAssetToDelete(null)
+    }
+
     return (
         <div className="modal-window-box">
-            <div className="modal-window-shadow clickable"/>
+            <div onClick={cancel}
+                 className="modal-window-shadow clickable"/>
             <div className={"confirm-delete-asset-window modal-window flex-box" +
             " flex-direction-column layer-1-themed-color"}>
                 <div className="confirm-delete-asset-window-headline flex-box flex-direction-row">
-                    <div className="confirm-delete-asset-window-close-button button neutral-button">x</div>
+                    <div onClick={cancel}
+                         className="confirm-delete-asset-window-close-button button neutral-button">x
+                    </div>
                 </div>
                 <div className="confirm-delete-asset-text text-label">{
                     `Delete ${assetToDelete.amount} ` + getCurrency(assetToDelete) + `${assetToDelete.name}?`
                 }</div>
                 <div className="confirm-delete-asset-buttons flex-box-centered flex-direction-row">
                     <div className="confirm-delete-asset-button button negative-button">Delete</div>
-                    <div className="confirm-delete-asset-button button neutral-button">Cancel</div>
+                    <div onClick={cancel}
+                         className="confirm-delete-asset-button button neutral-button">Cancel
+                    </div>
                 </div>
             </div>
         </div>
