b1fee7f9467f8a12b218fb96f4fcf3c1cbeef6ce
Serhii Maksymchuk <java.lead@prof-it.bz>
2023-07-28 21:47:07 +0300

    Added settings button without functionality yet

--- src/core/components/assets/AssetsManageButtons/index.js
+++ src/core/components/assets/AssetsManageButtons/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import './index.css';
 import PositiveButton from "../../buttons/PositiveButton";
+import NeutralButton from "../../buttons/NeutralButton";
+import SettingsIcon from './../../../../resources/images/icons8-settings.svg'
 
 export default function AssetsManageButtons(
     setShowCreateNewAssetWindow,
@@ -21,12 +23,17 @@ export default function AssetsManageButtons(
         setIsNewAssetNameInvalid(false);
     }
 
+    const showConfigsWindow = () => {
+    }
+
     return (
-        <div className="add-new-asset-row flex-box-centered flex-direction-row">
+        <div className="add-new-asset-row flex-box flex-direction-row">
+            <NeutralButton onClick={showConfigsWindow}
+                           className={"show-configs-window-button"}
+            ><SettingsIcon/></NeutralButton>
             <PositiveButton onClick={addNewAssetButtonClicked}
                             className={"add-new-asset-button layer-3-themed-color"}
             >+</PositiveButton>
-            <div className="add-new-asset-text text-label">Add new asset</div>
         </div>
     )
 }
