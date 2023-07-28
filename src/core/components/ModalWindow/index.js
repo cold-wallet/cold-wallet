ca66579d636f0c448408d9c4d7e8dc52372c30f8
Serhii Maksymchuk <java.lead@prof-it.bz>
2023-07-28 19:39:33 +0300

    Better look for modal window header

--- src/core/components/ModalWindow/index.js
+++ src/core/components/ModalWindow/index.js
@@ -17,7 +17,8 @@ export default function ModalWindow({onCancel, closeable, children, title, botto
             <div className={"modal-window modal-window-" + (large ? "large" : "small") + " flex-box" +
             " flex-direction-column layer-2-themed-color"}>
                 <div className={"modal-window-header modal-window-header-" + (large ? "large" : "small")
-                + " flex-box flex-direction-row layer-1-themed-color"}>
+                + " flex-box" + (closeable ? "" : " modal-window-header-centered") +
+                " flex-direction-row layer-1-themed-color"}>
                     {title}
                     {closeable ? CloseButton({onCancel}) : null}
                 </div>
