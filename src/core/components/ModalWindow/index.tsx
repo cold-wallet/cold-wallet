5438992305f5f996c495a6eeb12701bc7a6209a8
Serhii Maksymchuk <java.lead@prof-it.bz>
2023-08-25 17:41:19 +0300

    ModalWindow component migrated to typescript

--- src/core/components/ModalWindow/index.tsx
+++ src/core/components/ModalWindow/index.tsx
@@ -1,20 +1,31 @@
 import './index.css';
-import React from "react";
+import React, {JSX} from "react";
 import NeutralButton from "../buttons/NeutralButton";
 
-export default function ModalWindow({closeable, large, onCancel, children, title, bottom,}) {
+export default function ModalWindow(
+    {closeable, large, long, onCancel, children, title, bottom,}: {
+        closeable: boolean,
+        large: boolean,
+        long?: boolean,
+        onCancel?: () => void,
+        children: JSX.Element | JSX.Element[],
+        title?: string,
+        bottom?: JSX.Element | JSX.Element[] | null,
+    }
+) {
     const sizeSuffix = large ? "large" : "small";
     const onClickClose = () => {
         if (closeable) {
-            onCancel()
+            onCancel && onCancel()
         }
     };
     return (
         <div className="modal-window-box">
             <div onClick={onClickClose}
                  className={"modal-window-shadow" + (closeable ? " clickable" : "")}/>
-            <div className={"modal-window modal-window-" + sizeSuffix + " flex-box" +
-                " flex-direction-column layer-2-themed-color"}>
+            <div className={"modal-window modal-window-" + sizeSuffix + " flex-box"
+                + (long ? " modal-window-long" : "")
+                + " flex-direction-column layer-2-themed-color"}>
                 <div className={"modal-window-header modal-window-header-" + sizeSuffix
                     + " flex-box" + (closeable ? "" : " modal-window-header-centered") +
                     " flex-direction-row layer-1-themed-color"}>
