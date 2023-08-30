dc705e23c95bea5adccede773d9a75da9bd63c33
Serhii Maksymchuk <java.lead@prof-it.bz>
2023-08-30 12:29:38 +0300

    Optimised pin-code dial rendering

--- src/core/components/PinCode/Dial/index.tsx
+++ src/core/components/PinCode/Dial/index.tsx
@@ -1,8 +1,40 @@
 import './index.css'
-import {Dispatch, SetStateAction} from "react";
+import {Dispatch, SetStateAction, useMemo} from "react";
 
 export const PIN_CODE_MAX_SIZE = 8;
 export const PIN_CODE_MIN_SIZE = 4;
+
+interface DialButton {
+    symbol: string,
+    onClick: () => void,
+    activeOn: () => boolean,
+}
+
+const buildDialButtonData = (
+    index: number,
+    pinCode: string | null,
+    setPinCode: Dispatch<SetStateAction<string | null>>,
+) => ({
+    symbol: String(index),
+    onClick: () => {
+        if ((pinCode?.length || 0) + 1 <= PIN_CODE_MAX_SIZE) {
+            setPinCode((pinCode || "") + String(index))
+        }
+    },
+    activeOn: () => pinCode?.length || 0 < PIN_CODE_MAX_SIZE
+} as DialButton);
+
+const buildDialButton = (dialButton: DialButton) => (
+    <div key={dialButton.symbol}
+         onClick={() => {
+             if (dialButton.activeOn()) {
+                 dialButton.onClick()
+             }
+         }}
+         className={"dial-button pad layer-3-themed-color" + (dialButton.activeOn() ? "" : " disabled")}>
+        {dialButton.symbol}
+    </div>
+);
 
 export default function Dial(
     {props}: {
@@ -14,53 +46,31 @@ export default function Dial(
     }
 ) {
 
-    interface DialButton {
-        symbol: string,
-        onClick: () => void,
-        activeOn: () => boolean,
-    }
+    const dialButtons = useMemo(() => {
+        const dialButtonsData = [...Array(9)]
+            .map((__, i) => buildDialButtonData(i + 1, props.pinCode, props.setPinCode))
 
-    const buildDialButtonData = (index: number) => ({
-        symbol: String(index),
-        onClick: () => {
-            if ((props.pinCode?.length || 0) + 1 <= PIN_CODE_MAX_SIZE) {
-                props.setPinCode((props.pinCode || "") + String(index))
-            }
-        },
-        activeOn: () => props.pinCode?.length || 0 < PIN_CODE_MAX_SIZE
-    } as DialButton);
+        const dialButtons = dialButtonsData.map(buildDialButton)
+        dialButtons.push(buildDialButton({
+            symbol: '✓',
+            onClick: props.acceptPinCode,
+            activeOn: () => (!!(props.pinCode)
+                && props.pinCode.length <= PIN_CODE_MAX_SIZE
+                && props.pinCode.length >= PIN_CODE_MIN_SIZE)
+        }))
+        dialButtons.push(buildDialButton(buildDialButtonData(0, props.pinCode, props.setPinCode)))
+        dialButtons.push(buildDialButton({
+            symbol: '⌫',
+            onClick: () => {
+                if (props.pinCode?.length) {
+                    const pinCodeCut = props.pinCode.slice(0, -1);
+                    props.setPinCode(pinCodeCut || null)
+                }
+            },
+            activeOn: () => !!(props.pinCode?.length)
+        }))
+        return dialButtons
+    }, [props.pinCode])
 
-    const dialButtonsData = [...Array(9)].map((__, i) => buildDialButtonData(i + 1))
-
-    const buildDialButton = (dialButton: DialButton) => (
-        <div key={dialButton.symbol}
-             onClick={() => {
-                 if (dialButton.activeOn()) {
-                     dialButton.onClick()
-                 }
-             }}
-             className={"dial-button pad layer-3-themed-color" + (dialButton.activeOn() ? "" : " disabled")}>
-            {dialButton.symbol}
-        </div>
-    );
-    const dialButtons = dialButtonsData.map(buildDialButton)
-    dialButtons.push(buildDialButton({
-        symbol: '✓',
-        onClick: props.acceptPinCode,
-        activeOn: () => (!!(props.pinCode)
-            && props.pinCode.length <= PIN_CODE_MAX_SIZE
-            && props.pinCode.length >= PIN_CODE_MIN_SIZE)
-    }))
-    dialButtons.push(buildDialButton(buildDialButtonData(0)))
-    dialButtons.push(buildDialButton({
-        symbol: '⌫',
-        onClick: () => {
-            if (props.pinCode?.length) {
-                const pinCodeCut = props.pinCode.slice(0, -1);
-                props.setPinCode(pinCodeCut || null)
-            }
-        },
-        activeOn: () => !!(props.pinCode?.length)
-    }))
     return (<div className={"dial-container"}>{dialButtons}</div>)
 }
--- src/core/components/PinCode/index.tsx
+++ src/core/components/PinCode/index.tsx
@@ -1,6 +1,6 @@
 import './index.css'
 import Dial, {PIN_CODE_MAX_SIZE} from "./Dial";
-import React, {Dispatch, SetStateAction} from "react";
+import React, {Dispatch, SetStateAction, useMemo} from "react";
 
 export default function PinCode(
     {props}: {
@@ -11,13 +11,12 @@ export default function PinCode(
         }
     }
 ) {
+    const points = useMemo(() => [...Array(PIN_CODE_MAX_SIZE)].map((__, i) => (
+        <div key={i} className={"pin-code-point"
+            + ((props.pinCode?.length || 0) > i ? " pin-code-point--active" : "")}></div>
+    )), [props.pinCode]);
     return (<>
-        <div className="pin-code-points flex-box-centered flex-direction-row">{
-            [...Array(PIN_CODE_MAX_SIZE)].map((__, i) => (
-                <div key={i} className={"pin-code-point"
-                    + ((props.pinCode?.length || 0) > i ? " pin-code-point--active" : "")}></div>
-            ))
-        }</div>
+        <div className="pin-code-points flex-box-centered flex-direction-row">{points}</div>
         <Dial props={props}/>
     </>)
 }
