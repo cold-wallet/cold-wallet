a2187d397bb395b70dede57372fdfd62dc6d8f10
Serhii Maksymchuk <java.lead@prof-it.bz>
2023-09-12 15:58:44 +0300

    Cleanup

--- src/core/components/unauthorized/NotLoggedIn/index.tsx
+++ src/core/components/unauthorized/NotLoggedIn/index.tsx
@@ -8,47 +8,47 @@ import {dataImporter} from "../../settings/ImportData";
 import Props from "../../Props";
 
 export default function NotLoggedIn(props: Props) {
-    function buildImportPad() {
-        return (
-            <div className="import-box  flex-box-centered flex-direction-column">
+    return (
+        <div className={"startup-login-box layer-1-themed-color"}>
+            {props.importOrExportSettingRequested === null
+                ? <>
+                    {CreateWalletButton(props.setUserDataHolder,)}
+                    {ImportWalletButton(props.setImportOrExportSettingRequested,)}
+                </>
+                : buildImportPad(props)}
+        </div>
+    );
+}
+
+function buildImportPad(props: Props) {
+    return (
+        <div className="import-box  flex-box-centered flex-direction-column">
                 <textarea placeholder={"Copy text from 'Export'"}
                           onChange={event => props.setImportDataBuffer(event.target.value)}
                           style={{resize: 'none'}}
                           className={"import-text-area"}/>
-                <div className="import-box--controls flex-box-centered flex-direction-row">
-                    <PositiveButton
-                        onClick={() => {
-                            if (props.importDataBuffer) {
-                                dataImporter.readImportedData(props.importDataBuffer, props.setUserDataHolder)
-                                props.loadBinanceUserData()
-                                props.loadMonobankUserData()
-                                props.loadOkxUserData()
-                            }
-                            props.stateReset()
-                            props.setShowCreateNewAssetWindow(false)
-                            props.setCreatingNewAsset(false)
-                        }}
-                        className={"import-box--control"}
-                    >Import</PositiveButton>
-                    <NeutralButton
-                        onClick={() => {
-                            props.stateReset()
-                        }}
-                        className={"import-box--control"}
-                    >Cancel</NeutralButton>
-                </div>
+            <div className="import-box--controls flex-box-centered flex-direction-row">
+                <PositiveButton
+                    onClick={() => {
+                        if (props.importDataBuffer) {
+                            dataImporter.readImportedData(props.importDataBuffer, props.setUserDataHolder)
+                            props.loadBinanceUserData()
+                            props.loadMonobankUserData()
+                            props.loadOkxUserData()
+                        }
+                        props.stateReset()
+                        props.setShowCreateNewAssetWindow(false)
+                        props.setCreatingNewAsset(false)
+                    }}
+                    className={"import-box--control"}
+                >Import</PositiveButton>
+                <NeutralButton
+                    onClick={() => {
+                        props.stateReset()
+                    }}
+                    className={"import-box--control"}
+                >Cancel</NeutralButton>
             </div>
-        )
-    }
-
-    return (
-        <div className={"startup-login-box layer-1-themed-color"}>
-            {props.importOrExportSettingRequested === null
-                ? CreateWalletButton(props.setUserDataHolder)
-                : null /*LogInButton()*/}
-            {props.importOrExportSettingRequested === null
-                ? ImportWalletButton(props.setImportOrExportSettingRequested,)
-                : buildImportPad()}
         </div>
-    );
+    )
 }
