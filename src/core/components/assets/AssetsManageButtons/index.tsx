51715dd453b6803e3990498f5058b90dca0ff65a
Serhii Maksymchuk <java.lead@prof-it.bz>
2025-02-05 21:47:02 +0200

    Added total amount

--- src/core/components/assets/AssetsManageButtons/index.tsx
+++ src/core/components/assets/AssetsManageButtons/index.tsx
@@ -1,37 +1,28 @@
-import React, {Dispatch, SetStateAction} from "react";
+import React from "react";
 import './index.css';
 import PositiveButton from "../../buttons/PositiveButton";
 import NeutralButton from "../../buttons/NeutralButton";
 import SettingsIcon from './../../../../resources/images/icons8-settings.svg'
+import AssetsTotalAmount from "../AssetsTotalAmount";
+import Props from "../../Props";
 
-export default function AssetsManageButtons(
-    {
-        setShowCreateNewAssetWindow,
-        setCreatingNewAsset,
-        setShowConfigsWindow,
-        stateReset,
-    }: {
-        setShowCreateNewAssetWindow: Dispatch<SetStateAction<boolean>>,
-        setCreatingNewAsset: Dispatch<SetStateAction<boolean>>,
-        setShowConfigsWindow: Dispatch<SetStateAction<boolean>>,
-        stateReset: () => void,
-    }
-) {
+export default function AssetsManageButtons(props: Props) {
     const addNewAssetButtonClicked = () => {
-        stateReset();
-        setShowCreateNewAssetWindow(true);
-        setCreatingNewAsset(true);
+        props.stateReset();
+        props.setShowCreateNewAssetWindow(true);
+        props.setCreatingNewAsset(true);
     }
 
     const showConfigsWindow = () => {
-        stateReset();
-        setShowConfigsWindow(true);
+        props.stateReset();
+        props.setShowConfigsWindow(true);
     }
 
     return (<div className="add-new-asset-row flex-box flex-direction-row">
         <NeutralButton onClick={showConfigsWindow}
                        className={"show-configs-window-button"}
         ><SettingsIcon/></NeutralButton>
+        <AssetsTotalAmount props={props}/>
         <PositiveButton onClick={addNewAssetButtonClicked}
                         className={"add-new-asset-button layer-3-themed-color"}
         >+</PositiveButton>
---
+++ src/core/components/assets/AssetsTotalAmount/index.tsx
@@ -0,0 +1,58 @@
+import React, {useMemo} from "react";
+import './index.css';
+import Props from "../../Props";
+import {AccountInfo} from "../../../integrations/binance/binanceApiClient";
+import MonobankUserData from "../../../integrations/monobank/MonobankUserData";
+import {OkxAccount} from "../../../integrations/okx/okxApiClient";
+import noExponents from "../../../utils/noExponents";
+import NumberFormat from "react-number-format";
+
+export default function AssetsTotalAmount({props}: { props: Props }) {
+    const assetsTotal = useMemo(() => {
+            let assets = [...props.userData.assets];
+            if (props.userData.settings.binanceIntegrationEnabled) {
+                assets = assets.concat(AccountInfo.getAllAssets(props.binanceUserData))
+            }
+            if (props.userData.settings.monobankIntegrationEnabled) {
+                assets = assets.concat(MonobankUserData.getAllAssets(props.monobankUserData))
+            }
+            if (props.userData.settings.okxIntegrationEnabled) {
+                assets = assets.concat(OkxAccount.getAllAssets(props.okxUserData))
+            }
+            if (props.metaMaskSettingsEnabled && props.metaMaskAssets) {
+                assets = assets.concat((props.metaMaskAssets))
+            }
+            return Object.values(props.ccxtUserData)
+                .reduce((merged, current) => {
+                    return merged.concat(current)
+                }, assets)
+                .map(asset => {
+                    if (!asset.currency.replaceAll(" ", "")) {
+                        console.warn("no currency", asset)
+                    }
+                    return props.priceService.transform(asset.currency, +asset.amount, "USD")
+                })
+                .reduce((a, b) => +(a || 0) + +(b || 0))
+        },
+        [
+            props.userData,
+            props.monobankUserData,
+            props.binanceUserData,
+            props.okxUserData,
+        ]);
+
+    return (<div className={"assets-total-amount"}>
+        <div>Total: $<NumberFormat
+            allowLeadingZeros={false}
+            allowNegative={false}
+            isNumericString={true}
+            displayType={"text"}
+            decimalScale={2}
+            thousandSeparator={true}
+            value={noExponents(assetsTotal)}
+            //renderText={value => (
+            //    <div className={"assets-total-amount--text"}>{value}</div>
+            //)}
+        /></div>
+    </div>)
+}
